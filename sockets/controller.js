5856019401f25841bd3cd2710d050cd9dd08a11e
INDRA\edlopezh <edlopezh@indracompany.com>
2023-06-13 08:14:17 -0500

    app de colas finish

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,18 +1,54 @@
 
+const TicketControl = require('../models/ticket-control');
+
+const ticketControl = new TicketControl();
+
+
+
 
 const socketController = (socket) => {
 
-    console.log('Cliente conectado', socket.id);
+    //cuando un cliente se conecta
+    socket.emit('ultimo-ticket', ticketControl.ultimo);
+    socket.emit('estado-actual', ticketControl.ultimos4);
+    socket.emit('tickets-pendientes', ticketControl.tickets.length);
 
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id);
+    socket.on('siguiente-ticket', (payload, callback) => {
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente);
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
     });
 
-    socket.on('enviar-mensaje', (payload, callback) => {
-        const id = 123456789;
-        callback(id);
+    socket.on('atender-ticket', ({ escritorio }, callback) => {
 
-        socket.broadcast.emit('enviar-mensaje', payload);
+        socket.emit('estado-actual', ticketControl.ultimos4);
+
+        if (!escritorio) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.atenderTicket(escritorio);
+
+        
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+        socket.emit('tickets-pendientes', ticketControl.tickets.length);
+        socket.broadcast.emit('tickets-pendientes', ticketControl.tickets.length);
+
+        if (!ticket) {
+            callback({
+                ok: false,
+                msg: 'Ya no hay tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
+
     });
 }
 
